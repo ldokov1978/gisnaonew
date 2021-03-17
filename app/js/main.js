85472d2031a69fd543592b71e5f0827265a143c1
ldokov1978 <ldokov@mail.ru>
2021-03-17 10:20:45 +0300

    Добавлена обработка JSON

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,20 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    // Загрузка структуры приложений из файла JSON
+    const requestURL = '../json/structure.json';
+    const request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'text';
+    request.send();
+    request.onload = appsStructure;
 
+    function appsStructure() {
+        const main = document.querySelector(".main");
+        let mainHex = "";
+
+        let appsStruct = JSON.parse(request.response);
+        appsStruct.structure.forEach((items, i) => {
+            mainHex += '<div class="main-category-icon"><img src="../img/hex.svg" alt="hex" height="200" width="174"></div>'
+        });
+        main.insertAdjacentHTML ('beforeend', mainHex);
+    }
 });
